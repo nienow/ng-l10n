2c71a0153ceda417f2ce859c31c554c25bf7a46e
Matthew Nienow <rockopera8@gmail.com>
2020-05-23 19:51:37 -0500

    Clean up versions in package.json

--- projects/ngx-decimal/src/public-api.ts
+++ projects/ngx-decimal/src/public-api.ts
@@ -1,3 +1,8 @@
-export * from './lib/ngx-decimal.service';
-export * from './lib/ngx-decimal.directive';
+export { NgxDecimalService } from './lib/ngx-decimal.service';
+export { NgxDecimalDirective } from './lib/ngx-decimal.directive';
+export {
+  NgxDecimalComponent,
+  NgxDecimalPercentComponent,
+  NgxDecimalCurrencyComponent
+} from './lib/ngx-decimal.component';
 export * from './lib/ngx-decimal.module';
